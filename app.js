3f21de5682a75c0d67aa8d307bec1f4052b2709a
Kerwin Ejorango <lionelejorango@gmail.com>
2024-04-17 19:42:09 +0800

    fix(places-routes): addederror modal

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ const ROUTES_PLACES = require("./routes/routes-places");
 
 const app = express(); // creates an instance of express
 
-// handling of middlewares
+// list of middlewares
 app.use("/api/places", ROUTES_PLACES);
+
+// special: error handling middleware
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured!" });
+});
 
 // insert more here...
 
--- routes/routes-places.js
+++ routes/routes-places.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const HttpError = require("../models/http-error");
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -19,27 +21,31 @@ const DUMMY_PLACES = [
 // note: order matters when traversing through paths
 router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid;
+
   const place = DUMMY_PLACES.find((place) => {
     return place.id === placeId;
   });
+
   if (!place) {
-    return res
-      .status(404)
-      .json({ message: "No Place found for the provided place ID." });
+    throw new HttpError("No Place found for the provided place ID.", 404); //no need return
   }
+
   res.json({ place });
 });
 
 router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
+
   const place = DUMMY_PLACES.find((place) => {
     return place.creatorId === userId;
   });
+
   if (!place) {
-    return res
-      .status(404)
-      .json({ message: "No Place found for the provided user ID." });
+    return next(
+      new HttpError("No Place found for the provided user ID.", 404)
+    );
   }
+
   res.json({ place });
 });
 // insert more here...
