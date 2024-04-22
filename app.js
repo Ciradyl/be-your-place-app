cc8c51397d4147a1fcd5fa62a1f1eb1b5ce7813d
Kerwin Ejorango <lionelejorango@gmail.com>
2024-04-22 18:59:42 +0800

    init mongodb

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const HttpError = require("./models/http-error");
 const ROUTES_PLACES = require("./routes/routes-places");
-const ROUTES_USERS = require('./routes/routes-users')
+const ROUTES_USERS = require("./routes/routes-users");
 
 const app = express(); // creates an instance of express
 
 app.use(bodyParser.json());
 
 app.use("/api/places", ROUTES_PLACES);
-app.use("/api/users", ROUTES_USERS)
+app.use("/api/users", ROUTES_USERS);
 
 app.use((req, res, next) => {
   const error = new HttpError("Could not find this route", 404);
@@ -25,7 +26,15 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500);
   res.json({ message: error.message || "An unknown error occured!" });
 });
-
 // insert more here...
 
-app.listen(5000);
+mongoose
+  .connect(
+    "mongodb+srv://ghoul:falloutwinter@cluster0.fjqpomv.mongodb.net/?retryWrites=true&w=majority&appName=places"
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
