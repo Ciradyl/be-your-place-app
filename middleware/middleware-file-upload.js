5662dd7fea46e4c2e9a388bd4782a1c1a4ce620b
Kerwin Ejorango <lionelejorango@gmail.com>
2024-05-15 14:39:49 +0800

    feat: filter files to specific mime type

--- middleware/middleware-file-upload.js
+++ middleware/middleware-file-upload.js
@@ -1,5 +1,5 @@
 const multer = require("multer");
-const uuid = require("uuid/v1");
+const { v1: uuidv1 } = require("uuid");
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
@@ -8,16 +8,21 @@ const MIME_TYPE_MAP = {
 };
 
 const fileUpload = multer({
-  limits: 500000,
+  limits: 500000, // 500KB file size limit
   storage: multer.diskStorage({
     destination: (req, file, callback) => {
-      callback(null, "uploads/images");
+      callback(null, "uploads/images"); // Uploads folder
     },
     filename: (req, file, callback) => {
       const fileExtention = MIME_TYPE_MAP[file.mimetype];
-      callback(null, uuid() + "." + fileExtention);
+      callback(null, uuidv1() + "." + fileExtention); // Unique filename with extension
     },
   }),
+  fileFilter: (req, file, callback) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    let error = isValid ? null : new Error("Invalid mime type!"); // Only allow specified MIME types
+    callback(error, isValid);
+  },
 });
 
 module.exports = fileUpload;
